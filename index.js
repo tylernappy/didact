cb8d679cb022441901070345983fd571b0a5f990
Tyler Nappy <tylernappy@gmail.com>
2017-11-18 15:36:48 -0500

    bare minimum

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+function onConnection(socket) {
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
